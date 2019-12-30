c36b0882cff6c6351d62a3ce0f8159ddff0628f2
jaedong kim <jaedong27@gmail.com>
2019-12-30 18:30:36 +0900

    add function (reprojection Depth Image Points On Color Coordinate)

--- calipy/RGBDCamera.py
+++ calipy/RGBDCamera.py
@@ -38,3 +38,34 @@ class RGBDCamera():
 
     def translatePointsToDepthCoordinate(self, pointcloud):
         return self.transform_inv.translate(pointcloud)
+
+    def reprojectionDepthImagePointsOnColorCoordinate(self, pointcloud):
+        pointcloud_on_color = self.transform.move(pointcloud)
+        #print(points)
+        uvs = np.dot(self.color_camera.intrinsic, pointcloud_on_color)
+        uvs = uvs[:2,:] / uvs[2,:]
+        #print(uvs)
+        uvs = (uvs+0.5).astype(np.int)
+        #print(uvs)
+        #(points[,:] > 0)
+        valid_points = np.logical_and(np.logical_and(uvs[0,:] > 0, uvs[0,:] < self.color_camera.width), np.logical_and(uvs[1,:] > 0, uvs[1,:] < self.color_camera.height) )
+        depth_on_color = np.zeros((self.color_camera.height,self.color_camera.width,3))
+        uvs = np.flip(uvs,axis=0)
+        depth_on_color[tuple(uvs[:,valid_points].tolist())] = np.transpose(tuple(pointcloud[:,valid_points]))
+        # result_depth_on_color = depth_on_color.copy()
+        # valid_value = depth_on_color[:,:,2] > 0
+        # for v in range(2, depth_on_color.shape[0]-2):
+        #     for u in range(2,depth_on_color.shape[1]-2):
+        #         if valid_value[v,u]:
+        #             continue
+        #         region_mask = valid_value[v:v+5,u:u+5]
+        #         sum_value = np.sum(region_mask)
+        #         if sum_value == 0:
+        #             continue
+        #         #print(sum_value)
+        #         result_depth_on_color[v,u,0] = np.sum(depth_on_color[v:v+5,u:u+5,0]) / sum_value
+        #         result_depth_on_color[v,u,1] = np.sum(depth_on_color[v:v+5,u:u+5,1]) / sum_value
+        #         result_depth_on_color[v,u,2] = np.sum(depth_on_color[v:v+5,u:u+5,2]) / sum_value
+        #         #print(region_value)
+        
+        return depth_on_color
